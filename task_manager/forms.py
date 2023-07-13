695d2b04020f4f00e95b0501082bac319325196f
nikogamsahurdia <eleron96@gmail.com>
2023-07-13 12:46:52 +0300

    everythings functions work 18 test

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -8,11 +8,13 @@ from crispy_forms.helper import FormHelper
 class UserEditForm(UserChangeForm):
     first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'id': 'id_first_name'}))
     last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'id': 'id_last_name'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'id': 'id_username'}))
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'id': 'id_username'}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'id': 'id_password1'}))
+    password2 = forms.CharField(label='Подтверждение пароля', widget=forms.PasswordInput(attrs={'id': 'id_password2'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
 
 class UserPasswordChangeForm(PasswordChangeForm):
     class Meta:
@@ -38,10 +40,10 @@ class CreateUserForm(UserCreationForm):
             Submit('submit', 'Create', css_class='btn-primary')
         )
 
-class CustomPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    new_password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    new_password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+# class CustomPasswordChangeForm(PasswordChangeForm):
+#     old_password = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+#     new_password1 = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+#     new_password2 = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'class': 'form-control'}))
