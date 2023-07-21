ce647a71161f3a6c9f84c147a46951db25f41f09
nikogamsahurdia <eleron96@gmail.com>
2023-07-21 20:40:49 +0300

    refactoring users 4

--- task_manager/status/forms.py
+++ task_manager/status/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import Status
 
+
 class StatusForm(forms.ModelForm):
     class Meta:
         model = Status
         fields = ['name']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}),
+            'name': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Имя'}),
         }
--- task_manager/status/models.py
+++ task_manager/status/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(gettext_lazy('Name'), max_length=100)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
